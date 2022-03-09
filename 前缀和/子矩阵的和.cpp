e248fd75ccec1a7b4b9a6858b09df07becf5dfac
Gardenpriciple <96117411+Gardenpriciple@users.noreply.github.com>
2022-03-09 10:43:16 +0800

    Update 子矩阵的和.cpp

--- 前缀和/子矩阵的和.cpp
+++ 前缀和/子矩阵的和.cpp
@@ -5,28 +5,29 @@
 
 using namespace std;
 
-const int N = 100010;
+const int N = 1010;
 
-int n, m;
-int a[N]; //原数组
-int s[N]; //前缀和数组
+int n, m, q;
+int a[N][N], s[N][N];
 
 int main()
 {
-	scanf("%d%d", &n, &m);
+	scanf("%d%d%d", &n, &m, &q);
 
-	for (int i = 1; i <= n; i++)
-	{
-		scanf("%d", &a[i]);
-		s[i] = s[i - 1] + a[i];
-	}
+	for (int i = 1; i <= n; i ++)
+		for (int j = 1; j <= m; j++)
+		{
+			scanf("%d", &a[i][j]);
+			s[i][j] = s[i - 1][j - 1] + s[i][j - 1] + s[i - 1][j] + s[i - 1][j - 1];
+		}
 
-	while (m--)
+	while (q--)
 	{
-		int l, r;
-		scanf("%d%d", &l, &r);
-		printf("%d\n", s[r] - s[l - 1]);
+		int x1, x2, y1, y2;
+		scanf("%d%d%d%d", &x1, &x2, &y1, &y2);
+		printf("%d\n", s[x2][y2] - s[x1 - 1][y2] - s[x2][y1 - 1] + s[x1 - 1][y1 - 1]);
 	}
 
 	return 0;
 }
+
